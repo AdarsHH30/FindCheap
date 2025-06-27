a05d4fdc6809ad20272b1640413c38b57c92a81f
Amrith R Naik <amrithrnaik@gmail.com>
2025-06-27 21:43:02 +0530

    [init] removed react js and used nextjs for frontend - installed shadcn - inserted custom colors

--- Frontend/src/components/Navbar.jsx
+++
@@ -1,9 +0,0 @@
-<li className="navbar-login flex items-center px-4" style={{marginLeft: '3rem'}}>
-          <button
-            className="bg-green-500 text-white px-6 py-2 rounded-bl-full gap-2"
-            style={{marginLeft: '2rem'}}
-            onClick={() => (window.location.href = "/login")}
-          >
-            Login
-          </button>
-        </li>
